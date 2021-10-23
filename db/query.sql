62b3562c1f967ebc2d695bbee07012898c37d2be
Noah Blackman <nablackm@usc.edu>
2021-10-23 17:20:17 -0400

    added SQL commands to join for displaying Employees

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,11 @@
-SELECT role.id AS "Role ID", role.title AS "Role Title", department.name AS Department, role.salary FROM role
+SELECT role.id AS "Role ID", role.title AS "Role Title", department.name AS Department, role.salary AS Salary 
+FROM role
 INNER JOIN department ON role.department_id = department.id
 ORDER BY role.id ASC;
+
+SELECT employee.id AS "Employee ID", CONCAT (employee.first_name," ",employee.last_name) AS Employee, role.title AS "Role", department.name AS "Department", role.salary AS "Salary", CONCAT (manager.first_name," ",manager.last_name) AS Manager
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee Manager ON manager.id = employee.manager_id
+ORDER BY employee.id ASC;
